549d8593b5fa6e37dfb2769718b3f40f1882979e
Yusa0v0 <57658018+Yusa0v0@users.noreply.github.com>
2026-03-21 00:42:29 +0800

    Update config.js

--- src/static/js/config/config.js
+++ src/static/js/config/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
     API: {
         // VERSION: 'v1beta',
-        MODEL_NAME: 'gemini-2.0-flash-exp'
+        MODEL_NAME: 'gemini-3-flash-preview'
     },
     // You can change the system instruction to your liking
     SYSTEM_INSTRUCTION: {
